ff421b1e9db9455bde1fd7a67fb426a89fc50408
surunzi <surunzi@foxmail.com>
2017-12-21 23:37:15 +0800

    Dev: Init webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,23 @@
-module.exports = {
+var path = require('path');
 
+module.exports = {
+    entry: './src/index',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'allye.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
 };
